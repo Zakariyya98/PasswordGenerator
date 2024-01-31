7c5ce6686a0c49190a0b21ac3da0eb3eb4b5823d
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-01-31 19:09:42 +0000

    CharacterLength.jsx: add: Added the character count and placed it in a div above the range slider

--- src/components/CharacterLength/CharacterLength.jsx
+++ src/components/CharacterLength/CharacterLength.jsx
@@ -5,18 +5,24 @@ const CharacterLength = () => {
   const [rangeValue, setRangeValue] = useState(8);
 
   return (
-    <div>
-      <input
-        type="range"
-        id="character-length"
-        name="character-length"
-        min="2"
-        max="24"
-        step="1"
-        defaultValue={8}
-        onChange={(event) => setRangeValue(event.target.value)}
-      />
-      <p>Character Length {rangeValue}</p>
+    <div className="char-length__container">
+      <div className="char-length__range-value">
+        <h1>Character Length</h1>
+        <h1>{rangeValue}</h1>
+      </div>
+      <div>
+        <input
+          className="char-length__range"
+          type="range"
+          id="character-length"
+          name="character-length"
+          min="2"
+          max="24"
+          step="1"
+          defaultValue={8}
+          onChange={(event) => setRangeValue(event.target.value)}
+        />
+      </div>
     </div>
   );
 };
