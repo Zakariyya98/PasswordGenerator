7e6ee72f1abd0820a641d317fd18a0c0ad039e94
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-01-31 14:54:37 +0000

    CharacterLength.jsx: add: input range with a value that shows the current value

--- src/components/CharacterLength/CharacterLength.jsx
+++ src/components/CharacterLength/CharacterLength.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./CharacterLength.scss";
 
 const CharacterLength = () => {
+  const [rangeValue, setRangeValue] = useState(8);
+
   return (
     <div>
-      <h1>Character Length</h1>
+      <input
+        type="range"
+        id="character-length"
+        name="character-length"
+        min="2"
+        max="24"
+        step="1"
+        defaultValue={8}
+        onChange={(event) => setRangeValue(event.target.value)}
+      />
+      <p>Character Length {rangeValue}</p>
     </div>
   );
 };
