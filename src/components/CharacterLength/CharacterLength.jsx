4a1d7c8d75be5e41ef8e32c9692fb38b783b066a
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-05 18:08:49 +0000

    CharacterLength.jsx: add: wrapped within a fragment

--- src/components/CharacterLength/CharacterLength.jsx
+++ src/components/CharacterLength/CharacterLength.jsx
@@ -6,26 +6,30 @@ const CharacterLength = () => {
   const [rangeValue, setRangeValue] = useState(8);
 
   return (
-    <div className="char-length__container">
-      <div className="char-length__range-value">
-        <h1>Character Length</h1>
-        <h1>{rangeValue}</h1>
+    <>
+      <div className="char-length__container">
+        <div className="char-length__range-value">
+          <h1>Character Length</h1>
+          <h1>{rangeValue}</h1>
+        </div>
+        <div>
+          <input
+            className="char-length__range"
+            type="range"
+            id="character-length"
+            name="character-length"
+            min="2"
+            max="24"
+            step="1"
+            defaultValue={8}
+            onChange={(event) => setRangeValue(event.target.value)}
+          />
+        </div>
       </div>
       <div>
-        <input
-          className="char-length__range"
-          type="range"
-          id="character-length"
-          name="character-length"
-          min="2"
-          max="24"
-          step="1"
-          defaultValue={8}
-          onChange={(event) => setRangeValue(event.target.value)}
-        />
+        <GenerateButton charLength={rangeValue} />
       </div>
-      <GenerateButton charLength={rangeValue} />
-    </div>
+    </>
   );
 };
 
