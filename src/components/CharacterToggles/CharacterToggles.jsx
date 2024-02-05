109e0d1ac83f3524c1db58737df3fae719a30660
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-05 18:18:08 +0000

    CharacterToggles.*: created component

---
+++ src/components/CharacterToggles/CharacterToggles.jsx
@@ -0,0 +1,11 @@
+import "./CharacterToggles.scss";
+
+const CharacterToggles = () => {
+  return (
+    <div className="character-toggles__container">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default CharacterToggles;
