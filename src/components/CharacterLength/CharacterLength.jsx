ab40e969633a4817021cf052acbbe7f7b076cea1
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-05 16:38:41 +0000

    CharacterLength.jsx: remove: removed all generatePassword logic + the button

--- src/components/CharacterLength/CharacterLength.jsx
+++ src/components/CharacterLength/CharacterLength.jsx
@@ -4,26 +4,6 @@ import GenerateButton from "../GenerateButton";
 
 const CharacterLength = () => {
   const [rangeValue, setRangeValue] = useState(8);
-  const [generatedPassword, setGeneratedPassword] = useState("");
-
-  const Allowed = {
-    Lowers: "qwertyuiopasdfghjklzxcvbnm",
-    Numbers: "1234567890",
-  };
-
-  const getRandomCharFromString = (str) =>
-    str.charAt(Math.floor(Math.random() * str.length));
-
-  const generatePassword = (length) => {
-    let pwd = "";
-    // Could do a switch case here where the value of ticked boxes
-    // can generate the required combinations.
-    pwd += getRandomCharFromString(Allowed.Lowers);
-    pwd += getRandomCharFromString(Allowed.Numbers);
-    for (let i = pwd.length; i < length; i++)
-      pwd += getRandomCharFromString(Object.values(Allowed).join(""));
-    setGeneratedPassword(pwd);
-  };
 
   return (
     <div className="char-length__container">
@@ -45,8 +25,6 @@ const CharacterLength = () => {
         />
       </div>
       <GenerateButton />
-      <button onClick={() => generatePassword(rangeValue)}>Click me</button>
-      <p>Generated Password: {generatedPassword}</p>
     </div>
   );
 };
