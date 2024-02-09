7074c139c1dc6e723c8e15d5e40a6790f52c3a75
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-09 19:27:30 +0000

    CharacterToggles.jsx: removed the logic that generates the password and only handles the array that will hold the selected characters, this state is passed down into the GenerateButton Component

--- src/components/CharacterToggles/CharacterToggles.jsx
+++ src/components/CharacterToggles/CharacterToggles.jsx
@@ -1,24 +1,18 @@
 import { useState } from "react";
 import "./CharacterToggles.scss";
+import GenerateButton from "../GenerateButton";
 
-const CharacterToggles = () => {
+const CharacterToggles = (charLength) => {
   const [upperChecked, setUpperChecked] = useState(false);
   const [lowerChecked, setLowerChecked] = useState(false);
   const [numberChecked, setNumberChecked] = useState(false);
   const [symbolChecked, setSymbolChecked] = useState(false);
-
-  // Testing state for the returnedPasssword
-  const [returnedPassword, setReturnedPassword] = useState("");
 
   const characterArrays = {
     Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
     Lowers: "qwertyuiopasdfghjklzxcvbnm",
     Numbers: "1234567890",
     Symbols: "!@£$%^&*()_-+=:;<>",
-  };
-
-  const getRandomCharFromString = (str) => {
-    return str.charAt(Math.floor(Math.random() * str.length));
   };
 
   let passwordCharacters = [];
@@ -38,16 +32,6 @@ const CharacterToggles = () => {
   if (symbolChecked) {
     passwordCharacters.push(characterArrays.Symbols);
   }
-  // Move inside the function once working
-  let password = "";
-  const generatePasswordWithCheckedCharacters = (length) => {
-    // let password = "";
-    for (let i = password.length; i < length; i++)
-      password += getRandomCharFromString(
-        Object.values(passwordCharacters).join("")
-      );
-    setReturnedPassword(password);
-  };
 
   return (
     <div className="character-toggles__container">
@@ -83,12 +67,8 @@ const CharacterToggles = () => {
         <label>Include Symbols</label>
       </div>
 
-      <button onClick={() => generatePasswordWithCheckedCharacters(8)}>
-        Click me
-      </button>
-
       <div>
-        <p>Generated Password: {returnedPassword}</p>
+        <GenerateButton setChars={passwordCharacters} charLength={charLength} />
       </div>
     </div>
   );
