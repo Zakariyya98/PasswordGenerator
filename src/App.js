eb1aa7aa4f0bc7aa1d627b4c668aedc10d70ce42
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-09 20:10:31 +0000

    App.js: add: imported component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Card from "./components/Card";
+import GeneratedPassword from "./components/GeneratedPassword";
 
 function App() {
   return (
     <>
+      <GeneratedPassword />
       <Card />;
     </>
   );
