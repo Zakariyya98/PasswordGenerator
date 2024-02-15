90d3b11ddbc8437d9d226881282db4b61eb94733
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-15 22:27:26 +0000

    GeneratedPassword.jsx: add: added the ability to copy the password to the clipboard

--- src/components/GeneratedPassword/GeneratedPassword.jsx
+++ src/components/GeneratedPassword/GeneratedPassword.jsx
@@ -6,6 +6,11 @@ const GeneratedPassword = ({ generatedPassword }) => {
       <div className="generated-password__container">
         <div>
           <h1 className="generated-password__password">{generatedPassword}</h1>
+          <button
+            onClick={() => navigator.clipboard.writeText(generatedPassword)}
+          >
+            Copy Password
+          </button>
         </div>
       </div>
     </>
