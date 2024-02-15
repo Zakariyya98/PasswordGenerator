fbc4a10489cf7e41ea9ab55d0a58cb9d7cd27109
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-15 01:17:10 +0000

    app.js refactor: app.js now manages the state between the different components and becomes the common ancestor component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import "./App.css";
+import { useState } from "react";
+
 import Card from "./components/Card";
+import CharacterLength from "./components/CharacterLength";
 import GeneratedPassword from "./components/GeneratedPassword";
+import CharacterToggles from "./components/CharacterToggles";
+import GenerateButton from "./components/GenerateButton";
+
+import "./App.css";
 
 function App() {
+  // To allow the app to have a common ancestor component so the state can
+  // be passed between components I have rearranged the way the component
+  // state is managed.
+
+  const [rangeValue, setRangeValue] = useState(8);
+  const [charactersToggled, setCharactersToggled] = useState([]);
+
   return (
     <>
       <GeneratedPassword />
-      <Card />;
+      <Card>
+        <CharacterLength
+          rangeValue={rangeValue}
+          setRangeValue={setRangeValue}
+        />
+        <CharacterToggles
+          charactersToggled={charactersToggled}
+          setCharactersToggled={setCharactersToggled}
+          charLength={rangeValue}
+        />
+        <GenerateButton setChars={charactersToggled} charLength={rangeValue} />
+      </Card>
     </>
   );
 }
