a0e7b87730f9be734fcf404cd2548d4285bd13b7
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-20 22:08:43 +0000

    PasswordStrength.*: create: created new component

---
+++ src/components/PasswordStrength/PasswordStrength.jsx
@@ -0,0 +1,13 @@
+import "./PasswordStrength.scss";
+
+const PasswordStrength = () => {
+  return (
+    <>
+      <div className="password-strength__container">
+        <h1>Hello world</h1>
+      </div>
+    </>
+  );
+};
+
+export default PasswordStrength;
