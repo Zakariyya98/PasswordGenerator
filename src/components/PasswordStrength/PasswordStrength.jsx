0a45e506563c40fef4ef4217c49eb81834540a06
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-02-21 15:42:23 +0000

    PasswordStrength.*: add: quick mockup of how the component will look

--- src/components/PasswordStrength/PasswordStrength.jsx
+++ src/components/PasswordStrength/PasswordStrength.jsx
@@ -4,7 +4,16 @@ const PasswordStrength = () => {
   return (
     <>
       <div className="password-strength__container">
-        <h1>Hello world</h1>
+        <div className="password-strength__title">
+          <h1>Password Strength</h1>
+        </div>
+        <div className="password-strength__meter-container">
+          <p>Weak</p>
+          <div className="password-strength__meter"></div>
+          <div className="password-strength__meter"></div>
+          <div className="password-strength__meter"></div>
+          <div className="password-strength__meter"></div>
+        </div>
       </div>
     </>
   );
