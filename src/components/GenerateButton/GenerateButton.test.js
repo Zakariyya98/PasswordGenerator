2ab07d7c6b9f3b5e79d4d5272cd43a636e9666cb
Zakariyya Ahmed <36316601+Zakariyya98@users.noreply.github.com>
2024-03-27 14:52:51 +0000

    GenerateButton.test.js: add: test to generate the password to a given length

--- src/components/GenerateButton/GenerateButton.test.js
+++ src/components/GenerateButton/GenerateButton.test.js
@@ -1,6 +1,6 @@
 import GenerateButton from "./GenerateButton";
 
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 
 describe("Given the user is using the password generator", () => {
   describe("When the user has landed on a fresh instance of the app", () => {
@@ -12,6 +12,38 @@ describe("Given the user is using the password generator", () => {
       const generateButtonComponent = screen.getByTestId("generateButton");
       expect(generateButtonComponent).toBeInTheDocument;
     });
+  });
+
+  it("generates a password with specified length when clicked", () => {
+    const setChars = {
+      lowerCase: false,
+      upperCase: false,
+      numbers: false,
+      symbols: false,
+    };
+    const setGeneratedPasswordMock = jest.fn();
+    render(
+      <GenerateButton
+        charLength={8}
+        setChars={setChars}
+        setGeneratedPassword={setGeneratedPasswordMock}
+      />
+    );
+
+    const generateButton = screen.getByRole("button", {
+      name: /generate password/i,
+    });
+    fireEvent.click(generateButton);
+
+    console.log("Calls:", setGeneratedPasswordMock.mock.calls);
+    console.log(
+      "Generated Password:",
+      setGeneratedPasswordMock.mock.calls[0][0]
+    );
+
+    expect(setGeneratedPasswordMock).toHaveBeenCalledTimes(1);
+    expect(setGeneratedPasswordMock).toHaveBeenCalledWith(expect.any(String));
+    expect(setGeneratedPasswordMock.mock.calls[0][0]).toHaveLength(8);
   });
 });
 
